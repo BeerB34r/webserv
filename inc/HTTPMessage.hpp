bd8bb88e7e4eadd1901b27c419d4fe3a56986fbc
Mats de Beer <mde-beer@student.codam.nl>
2026-02-21 17:28:56 +0100

    HTTPMessage update
    has member containing the datas and fields split up a bit nicer for us
    the programmer.
    also has nicer prettyprinting :3

--- inc/HTTPMessage.hpp
+++ inc/HTTPMessage.hpp
@@ -28,8 +28,11 @@
 #ifndef HTTPMESSAGE_HPP
 # define HTTPMESSAGE_HPP
 
+#include <map>
 #include <optional>
+#include <set>
 #include <string>
+#include <variant>
 #include <vector>
 
 template <typename T>
@@ -37,6 +40,38 @@ using Maybe = std::optional<T>;
 
 class	HTTPMessage {
 public:
+	// subtypes
+	enum HTTPMethod {
+		GET,
+		HEAD,
+		POST,
+		PUT,
+		DELETE,
+		CONNECT,
+		OPTIONS,
+		TRACE,
+		PATCH,
+		MOVE,
+		COPY,
+		LINK,
+		UNLINK,
+		WRAPPED,
+		UNKNOWN,
+		UNSUPPORTED
+	};
+	struct RequestData {
+		// from startline
+		enum HTTPMethod	method;
+		std::string	requestTarget;
+		std::string	httpVersion;
+	};
+	struct ResponseData {
+		// from startline
+		std::string	httpVersion;
+		int			statusCode;
+		std::string	reasonPhrase;
+	};
+
 	HTTPMessage() = delete;
 	HTTPMessage(const std::string& startline, const std::vector<std::string>& fieldlines, const std::string& body) noexcept;
 	HTTPMessage(const HTTPMessage&) noexcept = default;
@@ -46,18 +81,25 @@ public:
 	auto	getStartline(void) const noexcept -> const std::string&;
 	auto	getFieldlines(void) const noexcept -> const std::vector<std::string>&;
 	auto	getBody(void) const noexcept -> const std::string&;
+	auto	isRequest(void) const noexcept -> bool;
+	auto	getData(void) const noexcept -> const std::variant<RequestData,ResponseData>&;
 	auto	prettyPrint(void) const noexcept -> const std::string;
+	static const std::set<HTTPMethod>	supportedRequestMethods;
 private:
-	std::string					startline;
-	std::vector<std::string>	fieldlines;
-	std::string					body;
-	
+	bool									requestStatus;
+	std::variant<RequestData,ResponseData>	data;
+	std::map<std::string,std::string>		fields;
+	// plumbing
+	std::string								startline;
+	std::vector<std::string>				fieldlines;
+	std::string								body;
 protected:
 };
 
 auto	readHTTPmessage(const std::string&) noexcept -> Maybe<HTTPMessage>;
 auto	readHTTPrequest(const std::string&) noexcept -> Maybe<HTTPMessage>;
 auto	readHTTPresponse(const std::string&) noexcept -> Maybe<HTTPMessage>;
+auto	toHTTPMethod(const std::string&) noexcept -> HTTPMessage::HTTPMethod;
 auto	operator<<(std::ostream&, const HTTPMessage&) noexcept -> std::ostream&;
 
 #endif // HTTPMESSAGE_HPP
--- src/HTTPMessage.cpp
+++ src/HTTPMessage.cpp
@@ -25,6 +25,7 @@
 /*   ——————————————————————————————                                           */
 /* ************************************************************************** */
 
+#include <iostream>
 #include <sstream>
 #include <HTTPMessage.hpp>
 #include <HTTPparsing.hpp>
@@ -32,12 +33,58 @@
 
 using String = std::string;
 
-HTTPMessage::HTTPMessage(const String& startline, const std::vector<String>& fieldlines, const String& body) noexcept
-: startline(startline), fieldlines(fieldlines), body(body) {}
+const std::set<HTTPMessage::HTTPMethod>	HTTPMessage::supportedRequestMethods = {
+	HTTPMessage::GET,
+	HTTPMessage::POST,
+	HTTPMessage::DELETE
+};
+
+static inline auto	toRequestMethod(const HTTPMessage::HTTPMethod& method) noexcept -> HTTPMessage::HTTPMethod{
+	return HTTPMessage::supportedRequestMethods.contains(method) ? method : HTTPMessage::UNSUPPORTED;
+}
+
+static inline auto	populateRequestData(const String& startline) noexcept -> HTTPMessage::RequestData {
+	String	method = startline.substr(0, startline.find(' '));
+	String	requestTarget = startline.substr(startline.find(' ') + 1, startline.rfind(' ') - (startline.find(' ') + 1));
+	String	httpVersion = startline.substr(startline.rfind(' ') + 1);
+	return {toRequestMethod(toHTTPMethod(method)), requestTarget, httpVersion};
+}
+
+static inline auto	populateFields(const std::vector<String>& fieldlines) -> std::map<String,String> {
+	std::map<String,String>	fields;
+	for (String s : fieldlines) {
+		String	key = s.substr(0, s.find(':'));
+		String	value = s.substr(s.find(':') + 1, s.size() - s.find(':'));
+		if (fields.contains(key)) {
+			fields[key] += "," + value;
+		} else fields[key] = value;
+	}
+	return fields;
+}
+
+static inline auto	populateResponseData(const String& startline) -> HTTPMessage::ResponseData {
+	String	httpVersion = startline.substr(0, startline.find(' '));
+	String	statusCode = startline.substr(startline.find(' ') + 1, 3);
+	String	reasonPhrase = startline.substr(startline.find(statusCode) + 4);
+	return {httpVersion, std::stoi(statusCode), reasonPhrase};
+}
+
+HTTPMessage::HTTPMessage(const String& startline, const std::vector<String>& fieldlines, const String& body) noexcept : startline(startline), fieldlines(fieldlines), body(body) {
+	if (HTTPparsing::requestLine(startline)) {
+		requestStatus = true;
+		data = populateRequestData(startline);
+		fields = populateFields(fieldlines);
+	} else {
+		requestStatus = false;
+		data = populateResponseData(startline);
+		fields = populateFields(fieldlines);
+	}
+}
 
 auto	HTTPMessage::getStartline(void) const noexcept -> const String& { return this->startline; }
 auto	HTTPMessage::getFieldlines(void) const noexcept -> const std::vector<String>& { return this->fieldlines; }
 auto	HTTPMessage::getBody(void) const noexcept -> const String& { return this->body; }
+auto	HTTPMessage::isRequest(void) const noexcept -> bool { return this->requestStatus; }
 
 auto	readHTTPmessage(const String& s) noexcept -> Maybe<HTTPMessage> {
 	Maybe<Parser<String>::resultType>	startlineParseRes = (HTTPparsing::startLine < HTTPparsing::crlf)(s);
@@ -54,7 +101,7 @@ auto	readHTTPmessage(const String& s) noexcept -> Maybe<HTTPMessage> {
   // statusLine == response
 
 
-// [TODO]: return some actual value so as to make the response accurate
+// [TODO]: return some actual value on failure so as to make the response accurate
 auto	readHTTPrequest(const String& s) noexcept -> Maybe<HTTPMessage> {
 	Maybe<Parser<String>::resultType>	requestlineParseRes = (HTTPparsing::requestLine < HTTPparsing::crlf)(s);
 	if (!requestlineParseRes) return std::nullopt; // from here on, everything is copied wholesale from readHTTPmessage()
@@ -83,11 +130,22 @@ auto	readHTTPresponse(const String& s) noexcept -> Maybe<HTTPMessage> {
 
 auto	HTTPMessage::prettyPrint(void) const noexcept -> const String {
 	std::stringstream	ss;
-	ss << "startline: \"" << this->getStartline() << "\"\n";
+	if (requestStatus) {
+		const RequestData	request = std::get<RequestData>(data);
+		ss << request.method << " request\n";
+		ss << "request target: \"" << request.requestTarget << "\"\n";
+		ss << "version: " << request.httpVersion << "\n";
+	} else {
+		const ResponseData	response = std::get<ResponseData>(data);
+		ss << "status " << response.statusCode << ", \"" << response.reasonPhrase << "\"\n";
+		ss << "version: " << response.httpVersion << "\n";
+	}
 	ss << "fieldlines: [\n";
-	for (const String& s : this->getFieldlines()) ss << "\t\"" << s << "\"\n";
+	for (const std::pair<String,String> s : fields) {
+		ss << "\t" << s.first << ": " << s.second << "\n";
+	}
 	ss << "]\n";
-	ss << "messagebody:\n\"" << this->getBody() << "\"\n";
+	ss << "messagebody (" << body.size() << "B):\n\"" << body << "\"\n";
 	return ss.str();
 }
 
@@ -98,4 +156,31 @@ auto	operator<<(std::ostream& os, const HTTPMessage& message) noexcept -> std::o
 	os << "\r\n";
 	os << message.getBody();
 	return os;
+}
+
+
+#define _methodPair(method) {#method, HTTPMessage::method}
+auto	toHTTPMethod(const String& s) noexcept -> HTTPMessage::HTTPMethod {
+	const static int	knownMethodCount = 14;
+	const static struct MethodPair { String s; HTTPMessage::HTTPMethod method; }	methodPairs[knownMethodCount] = {
+		_methodPair(GET),
+		_methodPair(HEAD),
+		_methodPair(POST),
+		_methodPair(PUT),
+		_methodPair(DELETE),
+		_methodPair(CONNECT),
+		_methodPair(OPTIONS),
+		_methodPair(TRACE),
+		_methodPair(PATCH),
+		_methodPair(MOVE),
+		_methodPair(COPY),
+		_methodPair(LINK),
+		_methodPair(UNLINK),
+		_methodPair(WRAPPED)
+	};
+
+	for (struct MethodPair pair : methodPairs) {
+		if (pair.s == s) return pair.method;
+	}
+	return HTTPMessage::UNKNOWN;
 }
