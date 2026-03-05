f8c5f71f7fc2f39833f93a853ebaff907bb9dffe
Mats de Beer <mde-beer@student.codam.nl>
2026-03-05 21:02:20 +0100

    cmdline moved to separate file

--- src/cmdline.cpp
+++ src/cmdline.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                            ::::::::        */
-/*   cmdline.hpp                                             :+:    :+:       */
+/*   cmdline.cpp                                             :+:    :+:       */
 /*                                                          +:+               */
 /*   By: mde-beer <mde-beer@student.codam.nl>              +#+                */
 /*                                                        +#+                 */
-/*   Created: 2026/03/05 17:41:43 by mde-beer            #+#    #+#           */
-/*   Updated: 2026/03/05 20:09:51 by mde-beer            ########   odam.nl   */
+/*   Created: 2026/03/05 20:08:33 by mde-beer            #+#    #+#           */
+/*   Updated: 2026/03/05 20:09:30 by mde-beer            ########   odam.nl   */
 /*                                                                            */
 /*   —————No norm compliance?——————                                           */
 /*   ⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝                                           */
@@ -25,29 +25,87 @@
 /*   ——————————————————————————————                                           */
 /* ************************************************************************** */
 
-#ifndef CMDLINE_HPP
-# define CMDLINE_HPP
+#include <iostream>
+#include <global.hpp>
+#include <cmdline.hpp>
+#include <getopt.h>
 
-#define CSI				"\033["
-#define RESET			CSI "0m"
-#define BOLD			CSI "1m"
-#define DIM				CSI "2m"
-#define ITALIC			CSI "3m"
-#define UNDERLINE		CSI "4m"
-#define BLINKING		CSI "5m"
-#define INVERSE			CSI "7m"
-#define HIDDEN 			CSI "8m"
-#define STRIKETHROUGH	CSI "9m"
-#define BLACK			CSI "30m"
-#define RED				CSI "31m"
-#define GREEN			CSI "32m"
-#define YELLOW			CSI "33m"
-#define BLUE			CSI "34m"
-#define MAGENTA			CSI "35m"
-#define CYAN			CSI "36m"
-#define WHITE			CSI "37m"
-#define DEFAULT			CSI "39m"
+auto	runTests(void) -> void;
 
-auto	cmdlineArgs(int& ac, char**& av) -> int;
+static inline auto	usage(const char* bin) -> void {
+	std::cout << CYAN BOLD "[USAGE]" RESET " " << bin << " [options] <config file>\n";
+	std::cout << CYAN BOLD "[OPTS]" RESET ":\n";
+	std::cout << "\t-h | --help | --usage\n";
+	std::cout << "\t\tshow this message\n";
+	std::cout << "\t-v | --verbose\n";
+	std::cout << "\t\tturn on verbose terminal output\n";
+	std::cout << "\t-l | --log\n";
+	std::cout << "\t\tturn on logging (default logfile: \"" << bin << ".log\")\n";
+	std::cout << "\t--logfile <logfile>\n";
+	std::cout << "\t\tspecify file for logging. implies '-l'\n";
+	std::cout << "\t-t | --tests\n";
+	std::cout << "\t\trun test suite\n";
+	// -f for fun is intentionally hidden :3
+}
 
-#endif // CMDLINE_HPP
+// returns 0 on success, 1 on happy exit, and 2 on unhappy exit
+auto	cmdlineArgs(int& ac, char**& av) -> int {
+	const char*	bin = av[0];
+	using namespace std::literals;
+	if (ac == 1) {
+		usage(av[0]);
+		return (1);
+	}
+	global::logfile = av[0] + ".log"s;
+	int				opt = 0;
+	int				opt_index = 0;
+	struct option	long_opts[] = {
+		{ "help", no_argument, nullptr,  'h' },
+		{ "usage", no_argument, nullptr,  'h' },
+		{ "verbose", no_argument, nullptr, 'v'},
+		{ "log", no_argument, nullptr, 'l'},
+		{ "logfile", required_argument, nullptr, 'o'},
+		{ "fun", no_argument, nullptr, 'f'},
+		{ "tests", no_argument, nullptr, 't'},
+		{} // trailing zero
+	};
+	while ((opt = getopt_long(ac, av, "hvlft", long_opts, &opt_index)) != -1) {
+		switch (opt) {
+			case 'h': {
+				usage(av[0]);
+				return (1);
+			}
+			case 'v': {
+				global::verbose = true;
+				break ;
+			}
+			case 'l': {
+				global::log = true;
+				break ;
+			}
+			case 'o': {
+				global::log = true;
+				global::logfile = optarg;
+				break ;
+			}
+			case 'f': {
+				global::fun_allowed = true;
+				break ;
+			}
+			case 't': {
+				runTests();
+				return (1);
+			}
+			default: {
+				return (2);
+			}
+		}
+	}
+	av = &av[optind];
+	ac -= optind;
+	if (ac != 1) {
+		usage(bin);
+		return (2);
+	}
+	return (0);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mde-beer <mde-beer@student.codam.nl>              +#+                */
 /*                                                        +#+                 */
 /*   Created: 2026/02/16 19:03:36 by mde-beer            #+#    #+#           */
-/*   Updated: 2026/03/05 20:07:07 by mde-beer            ########   odam.nl   */
+/*   Updated: 2026/03/05 20:09:55 by mde-beer            ########   odam.nl   */
 /*                                                                            */
 /*   —————No norm compliance?——————                                           */
 /*   ⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝                                           */
@@ -29,90 +29,9 @@
 #include <global.hpp>
 #include <cmdline.hpp>
 
-#include <getopt.h>
-
 // external routines
-auto	runTests(void) -> void;
 
-auto	usage(const char* bin) -> void {
-	std::cout << CYAN BOLD "[USAGE]" RESET " " << bin << " [options] <config file>\n";
-	std::cout << CYAN BOLD "[OPTS]" RESET ":\n";
-	std::cout << "\t-h | --help | --usage\n";
-	std::cout << "\t\tshow this message\n";
-	std::cout << "\t-v | --verbose\n";
-	std::cout << "\t\tturn on verbose terminal output\n";
-	std::cout << "\t-l | --log\n";
-	std::cout << "\t\tturn on logging (default logfile: \"" << bin << ".log\")\n";
-	std::cout << "\t--logfile <logfile>\n";
-	std::cout << "\t\tspecify file for logging. implies '-l'\n";
-	std::cout << "\t-t | --tests\n";
-	std::cout << "\t\trun test suite\n";
-	// -f for fun is intentionally hidden :3
-}
-
-// returns 0 on success, 1 on happy exit, and 2 on unhappy exit
-auto	cmdlineArgs(int& ac, char**& av) -> int {
-	const char*	bin = av[0];
-	using namespace std::literals;
-	if (ac == 1) {
-		usage(av[0]);
-		return (1);
-	}
-	global::logfile = av[0] + ".log"s;
-	int				opt = 0;
-	int				opt_index = 0;
-	struct option	long_opts[] = {
-		{ "help", no_argument, nullptr,  'h' },
-		{ "usage", no_argument, nullptr,  'h' },
-		{ "verbose", no_argument, nullptr, 'v'},
-		{ "log", no_argument, nullptr, 'l'},
-		{ "logfile", required_argument, nullptr, 'o'},
-		{ "fun", no_argument, nullptr, 'f'},
-		{ "tests", no_argument, nullptr, 't'},
-		{} // trailing zero
-	};
-	while ((opt = getopt_long(ac, av, "hvlft", long_opts, &opt_index)) != -1) {
-		switch (opt) {
-			case 'h': {
-				usage(av[0]);
-				return (1);
-			}
-			case 'v': {
-				global::verbose = true;
-				break ;
-			}
-			case 'l': {
-				global::log = true;
-				break ;
-			}
-			case 'o': {
-				global::log = true;
-				global::logfile = optarg;
-				break ;
-			}
-			case 'f': {
-				global::fun_allowed = true;
-				break ;
-			}
-			case 't': {
-				runTests();
-				return (1);
-			}
-			default: {
-				return (2);
-			}
-		}
-	}
-	av = &av[optind];
-	ac -= optind;
-	if (ac != 1) {
-		usage(bin);
-		return (2);
-	}
-	return (0);
-}
-
-auto	main([[maybe_unused]] int ac, [[maybe_unused]] char **av) -> int {
+auto	main(int ac, char **av) -> int {
 	if (int argres = cmdlineArgs(ac, av)) {
 		return (argres - 1);
 	}
